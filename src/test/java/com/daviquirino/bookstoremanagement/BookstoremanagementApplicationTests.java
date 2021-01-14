a7804e7ea3f098d28436c15124145fa1ca4460ef
Davi Quirino <davipires7@hotmail.com>
2021-01-14 08:13:39 -0300

    first commit

---
+++ src/test/java/com/daviquirino/bookstoremanagement/BookstoremanagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.daviquirino.bookstoremanagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookstoremanagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
